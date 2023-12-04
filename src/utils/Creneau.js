e16d63c6e6857253aecced0ee3ae14a2b96babf0
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-12-04 09:24:20 +0100

    revert: "fix(Creneau) : ordre des valeurs (#14)" (#15)
    This reverts commit cc4595f32e2590a0a104b79ffe5634f239c80267.

--- src/utils/Creneau.js
+++ src/utils/Creneau.js
@@ -1,5 +1,5 @@
 class Creneau {
-    constructor(ue, type, indexSousGroupe, horaire,salle, nbPlaces) {
+    constructor(ue, type, horaire, nbPlaces, salle, indexSousGroupe) {
         this.ue = ue;
         this.type = type;
         this.indexSousGroupe = indexSousGroupe;
