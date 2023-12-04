655067ef415cfd7b4f6abe5e323e59ada0de259a
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-12-04 09:54:07 +0100

    fix: strings and package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const colors = require('colors');
-const cli = require("@caporal/core").default;
+const cli = require('@caporal/core').default;
 
 const CreneauParser = require('./utils/CreneauParser.js');
 
@@ -28,6 +28,7 @@ cli
     .command('find-salles', 'Trouver les salles associées à un cours')
     .argument('<chemin>', 'Chemin du fichier ou du dossier contenant les créneaux')
     .argument('<cours>', 'Le cours à rechercher')
+    .alias('fs')
     .action(({args, options, logger}) => {
         let parser = new CreneauParser();
         parser.parse(args.chemin);
@@ -42,9 +43,10 @@ cli
     })
 
     // SPEC2
-    .command('capacite-max', 'Capacité max d\'une salle')
+    .command('capacite-max', "Capacité max d'une salle")
     .argument('<chemin>', 'Chemin du fichier ou du dossier contenant les créneaux')
     .argument('<salle>', 'La salle à rechercher')
+    .alias('cm')
     .action(({args, options, logger}) => {
         let parser = new CreneauParser();
         parser.parse(args.chemin);
@@ -56,12 +58,19 @@ cli
 
         let capacite = capaciteMax(parser.parsedCreneaux, args.salle);
         //console.log(capacite);
-        ;
     })
 
     // SPEC3
-    .command('verify-emploidutemps', 'Vérifier l\'emploi du temps')
+    .command('verify-emploidutemps', "Vérifier l'emploi du temps")
     //.argument('<fichier>', 'Le fichier à vérifier')
+    .alias('ve')
+    .action(({args, options, logger}) => {
+        // TODO
+    })
+
+    // SPEC6
+    .command('ical', "Créer un fichier iCal avec l'edt de l'utilisateur")
+    //.argument('<usager>', "La personne dont on veut l'emploi du temps") //
     .action(({args, options, logger}) => {
         // TODO
     })
